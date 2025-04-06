a521d12bed4918a95459aca647ca5370b2b0b03c
WizzCracker <165096450+WizzCracker@users.noreply.github.com>
2025-04-06 12:33:09 +0200

    Kontroler Client

---
+++ Projekt NET/Projekt NET/Controllers/ClientsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Projekt_NET.Models;
+
+namespace Projekt_NET.Controllers
+{
+    [Route("Klienci")]
+    public class ClientsController : Controller
+    {
+        private readonly DroneDbContext _context;
+
+        public ClientsController(DroneDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Clients
+        [Route("Main")]
+        public async Task<IActionResult> Index()
+        {
+            var droneDbContext = _context.Clients;
+            return View(await droneDbContext.ToListAsync());
+        }
+
+        // GET: Clients/Details/5
+        [Route("Szczegoly/{id}")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .Include(c => c.District)
+                .FirstOrDefaultAsync(m => m.ClientId == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // GET: Clients/Create
+        [HttpGet("Dodaj")]
+        public IActionResult Create()
+        {
+            ViewData["DistrictId"] = new SelectList(_context.Districts, "DistrictId", "Name");
+            return View();
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost("Dodaj")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ClientId,Name,Surname,PhoneNumber,DistrictId")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(client);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DistrictId"] = new SelectList(_context.Districts, "DistrictId", "Name", client.DistrictId);
+            return View(client);
+        }
+
+        // GET: Clients/Edit/5
+        [HttpGet("Edytuj/{id}")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+            ViewData["DistrictId"] = new SelectList(_context.Districts, "DistrictId", "Name", client.DistrictId);
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost("Edytuj/{id}")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ClientId,Name,Surname,PhoneNumber,DistrictId")] Client client)
+        {
+            if (id != client.ClientId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(client);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ClientExists(client.ClientId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["DistrictId"] = new SelectList(_context.Districts, "DistrictId", "Name", client.DistrictId);
+            return View(client);
+        }
+
+        // GET: Clients/Delete/5
+        [HttpGet("Usun/{id}")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Clients
+                .Include(c => c.District)
+                .FirstOrDefaultAsync(m => m.ClientId == id);
+            if (client == null)
+            {
+                return NotFound();
+            }
+
+            return View(client);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost("Usun/{id}"), ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var client = await _context.Clients.FindAsync(id);
+            if (client != null)
+            {
+                _context.Clients.Remove(client);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ClientExists(int id)
+        {
+            return _context.Clients.Any(e => e.ClientId == id);
+        }
+    }
+}
--- Projekt NET/Projekt NET/Controllers/HomeController.cs
+++ Projekt NET/Projekt NET/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace Projekt_NET.Controllers
             return View();
         }
 
+        public IActionResult Clients()
+        {
+            return RedirectToAction("Index", "Clients");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
