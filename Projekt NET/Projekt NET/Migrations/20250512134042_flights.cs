5e15278bdd13addd0a7771d3049c5ed3387e862f
DismantleMines <dismantleminesyessss@gmail.com>
2025-05-12 15:42:27 +0200

    migration

---
+++ Projekt NET/Projekt NET/Migrations/20250512134042_flights.cs
@@ -0,0 +1,360 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projekt_NET.Migrations
+{
+    /// <inheritdoc />
+    public partial class flights : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Districts",
+                columns: table => new
+                {
+                    DistrictId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Districts", x => x.DistrictId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlightPaths",
+                columns: table => new
+                {
+                    FlightPathId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlightPaths", x => x.FlightPathId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Models",
+                columns: table => new
+                {
+                    ModelId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaxCapacity = table.Column<int>(type: "int", nullable: false),
+                    MaxSpeed = table.Column<int>(type: "int", nullable: false),
+                    SpecialGear = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Models", x => x.ModelId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Operators",
+                columns: table => new
+                {
+                    OperatorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DroneCloudId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Operators", x => x.OperatorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Districts_BoundingPoints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Latitude = table.Column<double>(type: "float", nullable: false),
+                    Longitude = table.Column<double>(type: "float", nullable: false),
+                    DistrictId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Districts_BoundingPoints", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Districts_BoundingPoints_Districts_DistrictId",
+                        column: x => x.DistrictId,
+                        principalTable: "Districts",
+                        principalColumn: "DistrictId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DroneClouds",
+                columns: table => new
+                {
+                    DroneCloudId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DistrictId = table.Column<int>(type: "int", nullable: false),
+                    OperatorId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DroneClouds", x => x.DroneCloudId);
+                    table.ForeignKey(
+                        name: "FK_DroneClouds_Districts_DistrictId",
+                        column: x => x.DistrictId,
+                        principalTable: "Districts",
+                        principalColumn: "DistrictId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DroneClouds_Operators_OperatorId",
+                        column: x => x.OperatorId,
+                        principalTable: "Operators",
+                        principalColumn: "OperatorId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drones",
+                columns: table => new
+                {
+                    DroneId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CallSign = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    Coordinate_Latitude = table.Column<double>(type: "float", nullable: false),
+                    Coordinate_Longitude = table.Column<double>(type: "float", nullable: false),
+                    ModelId = table.Column<int>(type: "int", nullable: false),
+                    Range = table.Column<int>(type: "int", nullable: false),
+                    DroneCloudId = table.Column<int>(type: "int", nullable: true),
+                    AqDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drones", x => x.DroneId);
+                    table.ForeignKey(
+                        name: "FK_Drones_DroneClouds_DroneCloudId",
+                        column: x => x.DroneCloudId,
+                        principalTable: "DroneClouds",
+                        principalColumn: "DroneCloudId");
+                    table.ForeignKey(
+                        name: "FK_Drones_Models_ModelId",
+                        column: x => x.ModelId,
+                        principalTable: "Models",
+                        principalColumn: "ModelId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Flights",
+                columns: table => new
+                {
+                    FlightId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DepDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ArrivDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DroneId = table.Column<int>(type: "int", nullable: true),
+                    DeliveryCoordinates_Latitude = table.Column<double>(type: "float", nullable: false),
+                    DeliveryCoordinates_Longitude = table.Column<double>(type: "float", nullable: false),
+                    FlightPathId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flights", x => x.FlightId);
+                    table.ForeignKey(
+                        name: "FK_Flights_Drones_DroneId",
+                        column: x => x.DroneId,
+                        principalTable: "Drones",
+                        principalColumn: "DroneId");
+                    table.ForeignKey(
+                        name: "FK_Flights_FlightPaths_FlightPathId",
+                        column: x => x.FlightPathId,
+                        principalTable: "FlightPaths",
+                        principalColumn: "FlightPathId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Packages",
+                columns: table => new
+                {
+                    PackageId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientId = table.Column<int>(type: "int", nullable: false),
+                    DroneId = table.Column<int>(type: "int", nullable: true),
+                    Weight = table.Column<double>(type: "float", nullable: true),
+                    TargetAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PickupAddress = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packages", x => x.PackageId);
+                    table.ForeignKey(
+                        name: "FK_Packages_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "ClientId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Packages_Drones_DroneId",
+                        column: x => x.DroneId,
+                        principalTable: "Drones",
+                        principalColumn: "DroneId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deliveries",
+                columns: table => new
+                {
+                    DeliveryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    PackageId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deliveries", x => x.DeliveryId);
+                    table.ForeignKey(
+                        name: "FK_Deliveries_Packages_PackageId",
+                        column: x => x.PackageId,
+                        principalTable: "Packages",
+                        principalColumn: "PackageId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Deliverylogs",
+                columns: table => new
+                {
+                    DeliveryLogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PackageId = table.Column<int>(type: "int", nullable: false),
+                    LogDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    status = table.Column<int>(type: "int", nullable: false),
+                    Remarks = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Deliverylogs", x => x.DeliveryLogId);
+                    table.ForeignKey(
+                        name: "FK_Deliverylogs_Packages_PackageId",
+                        column: x => x.PackageId,
+                        principalTable: "Packages",
+                        principalColumn: "PackageId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliveries_PackageId",
+                table: "Deliveries",
+                column: "PackageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Deliverylogs_PackageId",
+                table: "Deliverylogs",
+                column: "PackageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Districts_BoundingPoints_DistrictId",
+                table: "Districts_BoundingPoints",
+                column: "DistrictId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DroneClouds_DistrictId",
+                table: "DroneClouds",
+                column: "DistrictId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DroneClouds_OperatorId",
+                table: "DroneClouds",
+                column: "OperatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drones_DroneCloudId",
+                table: "Drones",
+                column: "DroneCloudId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drones_ModelId",
+                table: "Drones",
+                column: "ModelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_DroneId",
+                table: "Flights",
+                column: "DroneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Flights_FlightPathId",
+                table: "Flights",
+                column: "FlightPathId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packages_ClientId",
+                table: "Packages",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packages_DroneId",
+                table: "Packages",
+                column: "DroneId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Deliveries");
+
+            migrationBuilder.DropTable(
+                name: "Deliverylogs");
+
+            migrationBuilder.DropTable(
+                name: "Districts_BoundingPoints");
+
+            migrationBuilder.DropTable(
+                name: "Flights");
+
+            migrationBuilder.DropTable(
+                name: "Packages");
+
+            migrationBuilder.DropTable(
+                name: "FlightPaths");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Drones");
+
+            migrationBuilder.DropTable(
+                name: "DroneClouds");
+
+            migrationBuilder.DropTable(
+                name: "Models");
+
+            migrationBuilder.DropTable(
+                name: "Districts");
+
+            migrationBuilder.DropTable(
+                name: "Operators");
+        }
+    }
+}
